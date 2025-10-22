d98027f900e6b8740230f4c25cdd8bb5d6e20cca
Afeef <afeefaaban747@gmail.com>
2025-10-22 18:22:34 +0530

    Working on enemy AI

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 const burst = preload("res://scenes/burst.tscn")
 const ERROR_SQ = 400.0 # 20
-
+const max_distance = 200
 
 @export var enemy_health : float
 @export_enum("loop", "linear") var patrol_type: String = "linear"
@@ -15,14 +15,17 @@ const ERROR_SQ = 400.0 # 20
 @onready var timer: Timer = $Timer
 @onready var hurt_anim: AnimationPlayer = $HurtAnim
 @onready var path_follow = get_parent()
+@onready var detector_col: CollisionShape2D = $Detector/Detector_col
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var raycast_pos_2: Marker2D = $RaycastPos2
+@onready var raycast_pos_1: Marker2D = $RaycastPos1
 
 
 var is_paused = false
-var player: CharacterBody2D = null
+var player
 var patrol_target : Vector2
 var patrol_index : int = 0
 var current_state : State
-
 enum State 
 {
 	Patrol,
@@ -31,16 +34,49 @@ enum State
 
 func _ready() -> void:
 	patrol_target = patrol_points[patrol_index]
-
-	
-	
+	player = get_tree().get_first_node_in_group("Player")
+	enter_state(State.Patrol)
 	
 func enter_state(new_state: State):
+	exit_state(current_state)
 	current_state = new_state
+	match State:
+		State.Patrol:
+			pass
+		State.Chase:
+			detector_col.disabled = false
+
+
+	
+func exit_state(old_state: State):
+	match old_state:
+		State.Patrol:
+			pass
+			#detector_col.disabled = true
+		State.Chase:
+			detector_col.disabled = false
+
+func _physics_process(delta: float) -> void:
+	
+	
+	if is_paused:
+		return  # Skip processing while paused
+	
+	if not is_on_floor():
+		velocity.y += get_gravity().y * delta
+		
+	if not ray_cast_2d.is_colliding() and is_on_floor():
+		
+		velocity.x = 0
+		print("hi")
+	if sprite.flip_h:
+		ray_cast_2d.global_position = raycast_pos_2.global_position
+	else:
+		ray_cast_2d.global_position = raycast_pos_1.global_position
 	match current_state:
 		State.Patrol:
 			var difference : Vector2 = patrol_target - global_position
-	#print(difference.length_squared())
+			#print(difference.length_squared())
 			if difference.length_squared() > ERROR_SQ:
 				var patrol_velocity = difference.normalized() * speed
 				velocity.x = patrol_velocity.x
@@ -49,25 +85,18 @@ func enter_state(new_state: State):
 				patrol_index = (patrol_index+1) % patrol_points.size()
 				patrol_target = patrol_points[patrol_index]
 		State.Chase:
-			pass
-func _physics_process(delta: float) -> void:
-	if is_paused:
-		return  # Skip processing while paused
-		
-	if not is_on_floor():
-		velocity.y += get_gravity().y * delta
-	
-	var difference : Vector2 = patrol_target - global_position
-	#print(difference.length_squared())
-	if difference.length_squared() > ERROR_SQ:
-		var patrol_velocity = difference.normalized() * speed
-		velocity.x = patrol_velocity.x
-		sprite.flip_h = velocity.x >0
-	else:
-		patrol_index = (patrol_index+1) % patrol_points.size()
-		patrol_target = patrol_points[patrol_index]
-	
-	move_and_slide()
+			var player_pos = player.global_position
+			if abs(player_pos.x - global_position.x) >max_distance:
+				enter_state(State.Patrol)
+			sprite.flip_h = velocity.x >0
+			var velocity_y = (player_pos -global_position).normalized()* speed
+			velocity.x = velocity_y.x
+			if ray_cast_2d.is_colliding():
+				pass
+			else:
+				velocity.x = -1
+			sprite.flip_h = player_pos.x-global_position.x > 0
+	#sprite.speed_scale =clamp(speed, 0.5, 3.0) 
 
 func take_damage(damage_val:float) -> void:
 	enemy_health -= damage_val
@@ -99,3 +128,8 @@ func _on_hurt_timer_timeout() -> void:
 func _on_enemy_body_entered(body) -> void:
 	if body.is_in_group("Player"):
 		body.apply_knockback(global_position)
+
+
+func _on_detector_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		enter_state(State.Chase)
