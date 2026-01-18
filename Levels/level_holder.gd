a5b1288e5f4913ee14173c2349e20c475266333e
Afeef <afeefaaban747@gmail.com>
2026-01-18 14:36:26 +0530

    working still on that logic and added placeholder levels and t areas

--- Levels/level_holder.gd
+++ Levels/level_holder.gd
@@ -5,11 +5,12 @@ extends Node2D
 var level_1 = load("res://Levels/level_1.tscn").instantiate()
 @onready var _1_2: Area2D = $"1-2"
 
-
+var loaded_areas : Array = [load("res://1_2.tscn").instantiate(), load("res://2_1.tscn").instantiate()]
 var to_load : Array
 var to_unload : Array
-var loaded : Array
-
+var loaded_levels : Array = [1]
+var t_areas_to_load : Array
+var t_areas_to_unload : Array
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#add_child(level_1)
@@ -18,7 +19,7 @@ func _ready() -> void:
 	_1_2.connect("entered",Callable(self,"on_entered"))
 
 func on_entered(from:int, to:int):
-	print("yes")
+	print("emit_succes")
 	print(from)
 	print(to)
 	to_load.clear()
@@ -36,25 +37,98 @@ func _load_n_unload():
 	for level in to_load:
 		var lvl_to_load = load("res://lvl_" + str(level)+ ".tscn").instantiate()
 		add_child(lvl_to_load)
-		loaded.append(level)
+		loaded_levels.append(level)
+		load_t_areas(level)
 	for level in to_unload:
-		var lvl_to_unload = load("res://lvl_" + str(level)+ ".tscn")
-		if level in loaded:
-			get_node("lvl"+str(level)).queue_free()
+		if level in loaded_levels:
+			var unload = get_node("lvl_1")
+			remove_child(unload)
+			unload_t_areas(level)
+			
 
 
 
+func load_t_areas(lvl):
 
+	
+	var t_area_1 = load("res://" + str(lvl)+"_"+ str(lvl + 1)+ ".tscn").instantiate()
+	var t_area_2 = load("res://" + str(lvl+1)+"_"+ str(lvl)+ ".tscn").instantiate()
+	t_areas_to_load.append(t_area_1)
+	t_areas_to_load.append(t_area_2)
+	if lvl>1:
+		var t_area_3 = load("res://" + str(lvl)+"_"+ str(lvl - 1)+ ".tscn").instantiate()
+		var t_area_4 = load("res://" + str(lvl-1)+"_"+ str(lvl)+ ".tscn").instantiate()
+		t_areas_to_load.append(t_area_3)
+		t_areas_to_load.append(t_area_4)
+	for area in t_areas_to_load:
+		if area in loaded_areas:
+			pass
+		else:
+			add_child(area)
+			loaded_areas.append(area)
+			area.connect("entered",Callable(self,"on_entered"))
 
-
-#func on_change():
-	#print(6)
-	#var current_scene = level_1.scene_file_path
-	#var next_lvl_num = current_scene.to_int() + 1
-	#var next_level_path = "res://Levels/level_" + str(next_lvl_num) + ".tscn"
-	#var next_level = load(next_level_path).instantiate()
-	#level_1.hide()
-	#remove_child(level_1)
-	#add_child(next_level)
-	#level_1 = next_level
-	#level_1.connect("change", Callable(self, "on_change"))
+func unload_t_areas(lvl):
+	var t_area_1 = get_node(str(lvl) +"-"+ str(lvl+1))
+	var t_area_2 = get_node(str(lvl+1) + "-"+str(lvl))
+	t_areas_to_unload.append(t_area_1)
+	t_areas_to_unload.append(t_area_2)
+	if lvl>1:
+		var t_area_3 = get_node(str(lvl) +"-"+ str(lvl-1))
+		var t_area_4 = get_node(str(lvl-1) +"-"+ str(lvl))
+		t_areas_to_unload.append(t_area_3)
+		t_areas_to_unload.append(t_area_4)
+	for area in t_areas_to_unload:
+		var area_path = area.scene_file_path
+		var area_loaded = load(area_path).instantiate() 
+		if area_loaded in loaded_areas:
+			if area in t_areas_to_load:
+				pass
+			else:
+				remove_child(area)
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+		#func on_change():
+			#print(6)
+			#var current_scene = level_1.scene_file_path
+			#var next_lvl_num = current_scene.to_int() + 1
+			#var next_level_path = "res://Levels/level_" + str(next_lvl_num) + ".tscn"
+			#var next_level = load(next_level_path).instantiate()
+			#level_1.hide()
+			#remove_child(level_1)
+			#add_child(next_level)
+			#level_1 = next_level
+			#level_1.connect("change", Callable(self, "on_change"))
--- t-area.gd
+++ t-area.gd
@@ -3,6 +3,10 @@ extends Area2D
 signal entered(from,to)
 @export var from : int
 @export var to : int
+
+
+func _ready() -> void:
+	body_entered.connect(Callable(self,"_on_body_entered"))
 func _on_body_entered(body: CharacterBody2D) -> void:
 	print("somene")
 	if body.is_in_group("player"):
