e1adb17822fe8f8e796a8243b411f89efa849136
Afeef <afeefaaban747@gmail.com>
2026-01-22 15:43:59 +0530

    added some art to pause menu

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -2,18 +2,23 @@ extends Control
 
 var paused : bool = false
 # Called when the node enters the scene tree for the first time.
+@onready var main_pause_menu: MarginContainer = $main_pause_menu
+@onready var setting_menu: MarginContainer = $setting_menu
 
 
 signal back_to_menu
 
 func _ready() -> void:
-	pass # Replace with function body.
+	main_pause_menu.show()
+	setting_menu.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	if Input.is_action_just_pressed("pause"):
+		main_pause_menu.show()
+		setting_menu.hide()	
 		pause_game()
 		
 func pause_game():
@@ -31,5 +36,16 @@ func _on_resume_pressed() -> void:
 
 
 func _on_quit_pressed() -> void:
+	get_tree().paused = false
 	print("emitting")
 	emit_signal("back_to_menu")
+
+
+func _on_settings_pressed() -> void:
+	main_pause_menu.hide()
+	setting_menu.show()
+
+
+func _on_back_pressed() -> void:
+	setting_menu.hide()
+	main_pause_menu.show()
--- scripts/core.gd
+++ scripts/core.gd
@@ -5,22 +5,24 @@ var cut_scene = load("res://scenes/Cutscene.tscn").instantiate()
 var level_1 = load("res://Levels/level_1.tscn").instantiate()
 var level_holder = load("res://Levels/level_holder.tscn").instantiate()
 var num = 1
-var pause_menu = load("res://scenes/pause_menu.tscn").instantiate()
+
 func _ready() -> void:
 	add_child(menu)
 	menu.connect("start_game", Callable(self,"on_menu_start_game"))
 	menu.connect("touch_control_toggle", Callable(self, "on_touch_control_toggled"))
 	cut_scene.connect("cut_scene_finished", Callable(self,"on_cut_scene_finished"))
-	pause_menu.connect("back_to_menu",Callable(self,"on_back_to_menu"))
+	
 
 func on_menu_start_game():
 	menu.visible = false
 	add_child(cut_scene)
+	
 
 func on_cut_scene_finished():
 	remove_child(cut_scene)
 	add_child(level_holder)
-	
+	var pause_menu = get_node("Level_holder").get_node("CanvasLayer").get_node("pause_menu")
+	pause_menu.connect("back_to_menu",Callable(self,"on_back_to_menu"))
 	
 #func on_touch_controls_toggled():
 	#while true:
@@ -28,9 +30,7 @@ func on_cut_scene_finished():
 			#print("YEA")
 	
 func on_back_to_menu():
-	print("recieved")
-	remove_child(level_holder)
-	menu.visible = true
+	get_tree().reload_current_scene()
 	
 
 	
